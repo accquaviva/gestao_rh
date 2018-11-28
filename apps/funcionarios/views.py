35803bd6bf67cca464e25c31b37bb30dd41ec646
lucas.cavalcante <lucasantos_cv@hotmail.com>
2018-11-28 10:23:36 -0200

    Funcionarios, filtrando pela empresa logada

--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,7 +1,12 @@
+from django.shortcuts import render
+
 from apps.funcionarios.models import Funcionario
 from django.views.generic.list import ListView
 
 class FuncionarioList(ListView):
     model = Funcionario
 
+    def get_queryset(self):
+        empresa_logada = self.request.user.funcionario.empresa
+        return Funcionario.objects.filter(empresa=empresa_logada)
 
