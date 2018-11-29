347f20f02498275f00da54423f0913ce18c6db40
lucas.cavalcante <lucasantos_cv@hotmail.com>
2018-11-29 09:13:34 -0200

    Ajuste onde lanço o documento atrelado ao usuário

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from apps.funcionarios.models import Funcionario
+from django.shortcuts import reverse
 
 class Documento(models.Model):
     descricao = models.CharField(max_length=100)
     pertence = models.ForeignKey(Funcionario,on_delete=models.PROTECT)
+
+    def get_absolute_url(self):
+        return reverse('list_funcionarios')
