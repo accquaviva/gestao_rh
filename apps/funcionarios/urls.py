af4f327103ec916d8b2d9159f2a3e85d0409f755
lucas.cavalcante <lucasantos_cv@hotmail.com>
2018-11-29 11:02:38 -0200

    Ajuste novo funcionario

--- apps/funcionarios/urls.py
+++ apps/funcionarios/urls.py
@@ -3,7 +3,7 @@ from .views import FuncionarioList,FuncionarioEdit,FuncionarioDelete,Funcionario
 
 urlpatterns = [
     path('', FuncionarioList.as_view(), name='list_funcionarios'),
-    path('novo', FuncionarioNovo.as_view(), name='novo_funcionarios'),
+    path('novo/', FuncionarioNovo.as_view(), name='novo_funcionarios'),
     path('editar/<int:pk>/',FuncionarioEdit.as_view(), name='update_funcionario'),
     path('deletar/<int:pk>/',FuncionarioDelete.as_view(), name='delete_funcionario'),
 ]
